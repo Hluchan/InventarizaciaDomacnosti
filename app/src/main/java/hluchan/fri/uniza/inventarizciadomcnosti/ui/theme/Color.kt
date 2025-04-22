9a71df9ccd7d17b6d109a86457c971080d57f290
Patrik <patrikhluchan@gmail.com>
2025-04-22 21:50:29 +0200

    Initial Commit

---
+++ app/src/main/java/hluchan/fri/uniza/inventarizciadomcnosti/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package hluchan.fri.uniza.inventarizciadomcnosti.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+// catpuccin color scheme, musim to zmenit na spravne farby
+val SkyLight = Color(0xFF04A5E5)
+val Sky = Color(0xFF0077B6)
+val Maroon = Color(0xFF800000)
+val Yellow = Color(0xFFFFD700)
+val Green = Color(0xFF008000)
+
+
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
